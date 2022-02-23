247247d1fdd662babbf727e9edff29a012a4b4bc
The0x539 <the0x539@gmail.com>
2022-02-23 15:55:59 -0600

    split into bin+lib crate pair

---
+++ src/lib.rs
@@ -0,0 +1,14 @@
+#![deny(rust_2018_idioms)]
+
+#[macro_use]
+mod util;
+
+mod cursor;
+mod drawing;
+mod input_handler;
+mod output_map;
+mod shell;
+mod state;
+pub mod udev;
+mod window_map;
+mod xwayland;
--- src/main.rs
+++ src/main.rs
@@ -4,22 +4,9 @@ use std::sync::Mutex;
 
 use slog::{Drain, Logger};
 
-#[macro_use]
-mod util;
-
-mod cursor;
-mod drawing;
-mod input_handler;
-mod output_map;
-mod shell;
-mod state;
-mod udev;
-mod window_map;
-mod xwayland;
-
 fn main() {
     let drain = Mutex::new(slog_term::term_full()).fuse();
     let log = Logger::root(drain, slog::o!());
 
-    udev::run(log);
+    wm::udev::run(log);
 }
