ce039a639a3b3a6c3dda8a661532578cbcbe2c44
The0x539 <the0x539@gmail.com>
2022-02-23 16:25:08 -0600

    log using slog-async

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,9 @@
 #![deny(rust_2018_idioms)]
 
-use std::sync::Mutex;
-
 use slog::{Drain, Logger};
 
 fn main() {
-    let drain = Mutex::new(slog_term::term_full()).fuse();
+    let drain = slog_async::Async::default(slog_term::term_full().fuse()).fuse();
     let log = Logger::root(drain, slog::o!());
 
     wm::udev::run(log);
