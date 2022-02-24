f71ee7eaa3eeb596acf114107db90dcd577937c1
The0x539 <the0x539@gmail.com>
2022-02-23 18:05:35 -0600

    implement XDG Decoration

--- src/state.rs
+++ src/state.rs
@@ -12,6 +12,7 @@ use smithay::wayland::{
     },
     output::xdg::init_xdg_output_manager,
     seat::{CursorImageStatus, KeyboardHandle, PointerHandle, Seat},
+    shell::xdg::decoration::{init_xdg_decoration_manager, XdgDecorationRequest},
     shm,
     tablet_manager::{init_tablet_manager_global, TabletSeatTrait},
     xdg_activation::{init_xdg_activation_global, XdgActivationEvent, XdgActivationState},
@@ -20,6 +21,7 @@ use smithay::xwayland::{XWayland, XWaylandEvent};
 
 use calloop::{generic::Generic, Interest, LoopHandle, Mode, PostAction};
 use slog::{error, info, Logger};
+use wayland_protocols::unstable::xdg_decoration;
 use wayland_server::{protocol::wl_surface::WlSurface, DispatchData, Display};
 
 use crate::{output_map::OutputMap, shell::ShellHandles, util::PseudoCell, window_map::WindowMap};
@@ -89,15 +91,15 @@ impl<B: Backend + 'static> State<B> {
 
         ShellHandles::init::<B>(display.clone(), log.clone());
 
-        init_xdg_output_manager(&mut display.borrow_mut(), log.clone());
-        init_xdg_activation_global(
-            &mut display.borrow_mut(),
-            xdg_activation_global_impl::<B>,
-            log.clone(),
-        );
+        let display_rc = display;
+        let mut display_ref = display_rc.borrow_mut();
+        let display = &mut *display_ref;
+
+        init_xdg_output_manager(display, log.clone());
+        init_xdg_activation_global(display, xdg_activation_global_impl::<B>, log.clone());
+        init_xdg_decoration_manager(display, xdg_decoration_impl, log.clone());
 
         let socket_name = if listen_on_socket {
-            let mut display = display.borrow_mut();
             let name = display.add_socket_auto().unwrap().into_string().unwrap();
             info!(log, "Listening on wayland socket"; "name" => &name);
             std::env::set_var("WAYLAND_DISPLAY", &name);
@@ -115,16 +117,11 @@ impl<B: Backend + 'static> State<B> {
                 DataDeviceEvent::DnDDropped => drag_icon.set(None),
                 _ => (),
             };
-            init_data_device(
-                &mut display.borrow_mut(),
-                cb,
-                default_action_chooser,
-                log.clone(),
-            );
+            init_data_device(display, cb, default_action_chooser, log.clone());
         }
 
         let seat_name = backend_data.seat_name();
-        let (mut seat, _) = Seat::new(&mut display.borrow_mut(), seat_name.clone(), log.clone());
+        let (mut seat, _) = Seat::new(display, seat_name.clone(), log.clone());
 
         let cursor_status = Rc::new(RefCell::new(CursorImageStatus::Default));
 
@@ -135,7 +132,7 @@ impl<B: Backend + 'static> State<B> {
         };
 
         {
-            init_tablet_manager_global(&mut display.borrow_mut());
+            init_tablet_manager_global(display);
             let cursor_status = cursor_status.clone();
             let cb = move |_tool: &_, status| cursor_status.set(status);
             seat.tablet_seat().on_cursor_surface(cb);
@@ -148,6 +145,9 @@ impl<B: Backend + 'static> State<B> {
             seat.add_keyboard(Default::default(), 200, 25, cb)
                 .expect("Failed to initialize the keyboard")
         };
+
+        drop(display_ref);
+        let display = display_rc;
 
         let xwayland = {
             let (xwayland, channel) = XWayland::new(handle.clone(), display.clone(), log.clone());
@@ -213,5 +213,21 @@ fn xdg_activation_global_impl<B: 'static>(
             }
         }
         XdgActivationEvent::DestroyActivationRequest { .. } => {}
+    }
+}
+
+fn xdg_decoration_impl(req: XdgDecorationRequest, _ddata: DispatchData<'_>) {
+    match req {
+        XdgDecorationRequest::NewToplevelDecoration { toplevel } => {
+            use xdg_decoration::v1::server::zxdg_toplevel_decoration_v1::Mode;
+
+            if toplevel
+                .with_pending_state(|state| state.decoration_mode = Some(Mode::ClientSide))
+                .is_ok()
+            {
+                toplevel.send_configure()
+            }
+        }
+        XdgDecorationRequest::SetMode { .. } | XdgDecorationRequest::UnsetMode { .. } => (),
     }
 }
